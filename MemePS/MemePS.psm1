4e2a4433dd76fab7ebf45223f195ebbadfdb63ea
corbob <corbob@knoxy.ca>
2020-03-10 20:22:56 -0700

    Moar stuffs

---
+++ MemePS/MemePS.psm1
@@ -0,0 +1,17 @@
+function Get-Process {
+	Write-Error "You should be running pls..."
+}
+
+Set-Alias -Name pls -Value Microsoft.PowerShell.Management\Get-Process
+
+function Stop-Process {
+	Write-Error "Yo dawg, I heard you liked processes... You need to use stahp..."
+}
+
+Set-Alias -Name stahp -Value Microsoft.PowerShell.Management\Stop-Process
+
+function Add-Content {
+	Write-Error "Did you think you could add content this way? MoarStuffs plz"
+}
+
+Set-Alias -Name MoarStuffs -Value Microsoft.PowerShell.Management\Add-Content
